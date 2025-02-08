01aca996a6e529da9fea122d53d2f86a61b3aba0
susumu <kosu.1024@gmail.com>
2025-02-08 18:22:45 +0900

    Next.js静的サイトとしてデプロイ

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/susu-tech.github.io",
+  assetPrefix: "/susu-tech.github.io",
+  output: "export",
+};
+
+export default nextConfig;
